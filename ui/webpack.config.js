027e0cced09b6dc16cf4c1ea9cbb88f2374f4d5c
datageek00 <wsubhankarb@gmail.com>
2023-09-29 10:31:46 +0530

    feat: cli integration with html report

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../datachecks/dashboard/static/'),
+        path: path.resolve(__dirname, '../datachecks/report/static/'),
     },
     optimization: {
         minimize: true,
