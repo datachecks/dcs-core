21df7356c1fad1a0482302d31d1111ef38e6b4fb
datageek00 <wsubhankarb@gmail.com>
2023-10-16 07:56:57 +0530

    fix: cleanup dependency and tests

--- tests/integration/datasource/test_elasticsearch_datasource.py
+++ tests/integration/datasource/test_elasticsearch_datasource.py
@@ -11,9 +11,3 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
-import pytest
-
-from datachecks.core.common.errors import DataChecksDataSourcesConnectionError
-from datachecks.integrations.databases.elasticsearch import ElasticSearchDataSource
-from datachecks.integrations.databases.opensearch import OpenSearchDataSource
