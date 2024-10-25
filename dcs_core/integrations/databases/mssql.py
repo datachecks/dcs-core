9f28f1ef1d0d290464b77800f291c978bc59f8c4
Neeraj Kumar <neerajkr1210@gmail.com>
2024-10-25 18:49:52 +0530

    fix: custom regex query for mssql,mysql and db2

--- dcs_core/integrations/databases/mssql.py
+++ dcs_core/integrations/databases/mssql.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple
 
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
@@ -59,3 +59,150 @@ class MssqlDataSource(SQLDataSource):
             raise DataChecksDataSourcesConnectionError(
                 message=f"Failed to connect to Mssql data source: [{str(e)}]"
             )
+
+    def regex_to_sql_condition(self, regex_pattern: str, field: str) -> str:
+        """
+        Convert regex patterns to SQL Server conditions
+        """
+        if (
+            regex_pattern.startswith("^") and regex_pattern.endswith("$")
+        ) or "|" in regex_pattern:
+            pattern = regex_pattern.strip("^$")
+            if pattern.startswith("(") and pattern.endswith(")"):
+                pattern = pattern[1:-1]
+
+            if "|" in pattern:
+                values = [f"'{val.strip()}'" for val in pattern.split("|")]
+                return f"IIF({field} IN ({', '.join(values)}), 1, 0)"
+
+        pattern = regex_pattern
+        if pattern.startswith("^"):
+            pattern = pattern[1:]
+        if pattern.endswith("$"):
+            pattern = pattern[:-1]
+
+        pattern = pattern.replace(".*", "%").replace(".+", "%").replace(".", "_")
+
+        return f"IIF({field} LIKE '{pattern}', 1, 0)"
+
+    def query_string_pattern_validity(
+        self,
+        table: str,
+        field: str,
+        regex_pattern: str = None,
+        predefined_regex_pattern: str = None,
+        filters: str = None,
+    ) -> Tuple[int, int]:
+        """
+        Get the count of valid values based on the regex pattern.
+        :param table: table name
+        :param field: column name
+        :param regex_pattern: custom regex pattern
+        :param predefined_regex_pattern: predefined regex pattern
+        :param filters: filter condition
+        :return: count of valid values, count of total row count
+        """
+        filters = f"WHERE {filters}" if filters else ""
+        qualified_table_name = self.qualified_table_name(table)
+        if not regex_pattern and not predefined_regex_pattern:
+            raise ValueError(
+                "Either regex_pattern or predefined_regex_pattern should be provided"
+            )
+        if regex_pattern:
+            regex = regex_pattern
+        else:
+            regex = self.regex_patterns[predefined_regex_pattern]
+
+        regex = self.regex_to_sql_condition(regex, field)
+
+        query = f"""
+            SELECT SUM(CAST({regex} AS BIGINT)) AS valid_count,
+                   COUNT(*) AS total_count
+            FROM {qualified_table_name}
+            {filters}
+        """
+
+        result = self.fetchone(query)
+        return result[0], result[1]
+
+    def query_valid_invalid_values_validity(
+        self,
+        table: str,
+        field: str,
+        regex_pattern: str = None,
+        filters: str = None,
+        values: List[str] = None,
+    ) -> Tuple[int, int]:
+        """
+        Get the count of valid and invalid values for a specified column.
+        :param table: table name
+        :param field: column name
+        :param values: list of valid values
+        :param regex_pattern: regex pattern (will be converted to SQL Server pattern)
+        :param filters: filter condition
+        :return: count of valid values and total count of rows.
+        """
+        filters = f"WHERE {filters}" if filters else ""
+        qualified_table_name = self.qualified_table_name(table)
+
+        if values:
+            values_str = ", ".join([f"'{value}'" for value in values])
+            validity_condition = f"IIF({field} IN ({values_str}), 1, 0)"
+        elif regex_pattern:
+            validity_condition = self.regex_to_sql_condition(regex_pattern, field)
+        else:
+            raise ValueError("Either 'values' or 'regex_pattern' must be provided.")
+
+        query = f"""
+            SELECT SUM(CAST({validity_condition} AS BIGINT)) AS valid_count,
+                   COUNT(*) AS total_count
+            FROM {qualified_table_name}
+            {filters}
+        """
+
+        result = self.fetchone(query)
+        return result[0], result[1]
+
+    def query_get_usa_state_code_validity(
+        self, table: str, field: str, filters: str = None
+    ) -> Tuple[int, int]:
+        """
+        Get the count of valid USA state codes
+        :param table: table name
+        :param field: column name
+        :param filters: filter condition
+        :return: count of valid state codes, count of total row count
+        """
+        valid_state_codes_str = ", ".join(
+            f"'{code}'" for code in self.valid_state_codes
+        )
+
+        filters = f"WHERE {filters}" if filters else ""
+        qualified_table_name = self.qualified_table_name(table)
+
+        regex_query = f"""
+                CASE
+                    WHEN {field} IS NULL THEN 0
+                    WHEN {field} IN ({valid_state_codes_str})
+                    THEN 1
+                    ELSE 0
+                END"""
+
+        query = f"""
+                SELECT
+                    SUM(CAST({regex_query} AS BIGINT)) AS valid_count,
+                    COUNT(*) AS total_count
+                FROM {qualified_table_name}
+                {filters}
+            """
+        result = self.fetchone(query)
+        return result[0], result[1]
+
+    def query_timestamp_metric(self):
+        raise NotImplementedError("Method not implemented for MssqlDataSource")
+
+    def query_timestamp_not_in_future_metric(self):
+        raise NotImplementedError("Method not implemented for MssqlDataSource")
+
+    def query_timestamp_date_not_in_future_metric(self):
+        raise NotImplementedError("Method not implemented for MssqlDataSource")
