4a1756c362a11cd5b25c691684b6dd1a852c783f
Ashank Priyadarshi <61231703+ashank2603@users.noreply.github.com>
2025-01-06 12:10:42 +0530

    fix: error logging

--- dcs_core/integrations/databases/sybase.py
+++ dcs_core/integrations/databases/sybase.py
@@ -18,6 +18,7 @@ import time
 from datetime import datetime
 from typing import Any, Dict, List, Tuple, Union
 
+from loguru import logger
 from sqlalchemy import create_engine
 
 from dcs_core.core.common.errors import DataChecksDataSourcesConnectionError
@@ -433,7 +434,7 @@ class SybaseDataSource(SQLDataSource):
 
                 return valid_count, total_count
             except Exception as e:
-                print(f"Error occurred: {e}")
+                logger.error(f"Error occurred: {e}")
                 return 0, 0
         else:
             raise ValueError(f"Unknown predefined regex pattern: {predefined_regex}")
@@ -499,7 +500,7 @@ class SybaseDataSource(SQLDataSource):
 
             return valid_count, total_count
         except Exception as e:
-            print(f"Error occurred: {e}")
+            logger.error(f"Error occurred: {e}")
             return 0, 0
 
     def query_timestamp_date_not_in_future_metric(
@@ -559,5 +560,5 @@ class SybaseDataSource(SQLDataSource):
 
             return valid_count, total_count
         except Exception as e:
-            print(f"Error occurred: {e}")
+            logger.error(f"Error occurred: {e}")
             return 0, 0
